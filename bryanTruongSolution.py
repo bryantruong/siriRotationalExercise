c726ebcb8edd210e9b69f5030dd24d6a7fefed4c
bryantruong <btruong@live.unc.edu>
2020-02-23 15:41:17 -0500

    Completed Solution

--- bryanTruongSolution.py
+++ bryanTruongSolution.py
@@ -1,61 +1,91 @@
 """
 Bryan Truong
-2/21/2020
+2/23/2020
 Completed for Siri Rotational Engineer Program Interview(s)
 """
-import numpy as np
 from nltk.corpus import words
-from collections import defaultdict
 
-englishWords = words.words()
+dictionary = words.words()
 # All possible words that are 3 characters or greater
-englishWords = [w for w in englishWords if len(w) >= 3]
+dictionary = [w for w in dictionary if len(w) >= 3]
+board = [
+    ["r", "a", "e", "l"],
+    ["m", "o", "f", "s"],
+    ["t", "e", "o", "k"],
+    ["n", "a", "t", "i"],
+]
+deltas = [(-1, -1),  # Top left neighbor
+          (-1, 0),  # Move up
+          (-1, 1),  # Top right neighbor
+          (0, 1),  # Move right
+          (1, 1),  # Bottom right neighbor
+          (1, 0),  # Move down
+          (1, -1),  # Bottom left neighbor
+          (0, -1)  # Move left
+          ]
 
 
-class Trie(object):
-    # Each "node" is a dictionary, with the key being the letter and the value being the neighboring children
-    def __init__(self):
-        self.children = defaultdict(Trie)  # Use default dict so that if the child doesn't exist, it is created
-        self.complete = False  # Use a complete flag to save time on search
+class Trie:  # Use nested dictionaries to implement a trie
+    root = {}  # The dict will store the neighbors
 
-    def addWord(self, word):
-        if not word:  # Base case (empty string case)
-            self.complete = True
-        else:
-            # Recursively call addWord with the substring after the first letter
-            self.children[word[0]].addWord(word[1:])
+    def add(self, wordToAdd):
+        current = self.root
+        for character in wordToAdd:
+            if character not in current:  # search the keys of the dict
+                current[character] = {}  # initialize that new node's dictionary
+            current = current[character]  # Move the pointer to the prefix
+        current['*'] = True  # Use '*' to represent the end of a word
 
-    def checkWord(self, word, prefixCheck = False):
-        if not word:  # Base case (empty string case)
-            return prefixCheck or self.complete  # If it was part of a valid word, self.complete will be True
-        else:
-            if word[0] in self.children:  # only do the check if the next letter is a child
-                return self.chilrden[word[0]].checkWord(word[1:], prefixCheck)
-            else:  # if the next letter is a child, the word isn't valid
+    def search(self, queryWord):
+        current = self.root
+        for character in queryWord:
+            if character not in current:
                 return False
-
-    # Sanity check to see if trie is working correctly
-    def showTrie(self, indent=0):
-        for child in self.children:
-            print(str(child), self.children[child].complete)
-            # Print that child's children
-            self.children[child].showTrie(indent=indent+1)
-
+            current = current[character]
+        # After moving to the last character node, check if it terminates with the '*'
+        if '*' in current:
+            return True
+        else:
+            return False
 
 
+def dfs(startingCell, currentWord, visited, trieNode):
+    if startingCell in visited:
+        return
+    letter = board[startingCell[0]][startingCell[1]]
+    visited.append(startingCell)
+
+    if letter in trieNode:
+        currentWord += letter
+        if '*' in trieNode[letter]:  # Signifies that this was the end of a word
+            print(currentWord)
+        neighbors = getNeighbors(startingCell)
+        for neighbor in neighbors:
+            # Recurse with the neighboring coordinates and the corresponding trie node
+            dfs(neighbor, currentWord, visited, trieNode[letter])
 
 
-
-
-# Use a trie (prefix tree) to represent possible words
-# def build_dictionary_trie(wordBank, node):
-#     for word in wordBank:
-#         if word[0] not in node:
+def getNeighbors(startingCoordinates):
+    neighboringCoordinates = []
+    for delta in deltas:
+        newRowCoord = startingCoordinates[0] + delta[0]
+        newColCoord = startingCoordinates[1] + delta[1]
+        if newRowCoord < 0 or newRowCoord > 3:
+            continue
+        if newColCoord < 0 or newColCoord > 3:
+            continue
+        neighboringCoordinates.append((newRowCoord, newColCoord))
+    return neighboringCoordinates
 
 
 if __name__ == '__main__':
     root = Trie()
-    dictionary = ["and", "an", "ant", "cat"]
+    # Create trie
     for word in dictionary:
-        root.addWord(word)
-    root.showTrie()
+        root.add(word)
+    # Begin DFS on each cell
+    print("Found Words:")
+    for row in range(4):
+        for col in range(4):
+            currentLetterCoords = (row, col)
+            dfs(currentLetterCoords, "", [], root.root)
